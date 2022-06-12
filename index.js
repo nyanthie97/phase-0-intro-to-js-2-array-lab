f9af97a276f87341e0aa9053b7249ae023367691
Nyanthie Mayang Gatluak <nyanthie.mayang@student.moringaschool.com>
2022-06-12 15:44:44 +0300

    commit tree 3320624aea362efeb5439c0a0c1b7d826bf55382

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+});
+
+  function destructivelyAppendCat(){
+      cats.push('Ralph')
+};
+    function destructivelyPrependCat(){
+        cats.unshift('Bob')
+    };
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+};
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+};
+
+function appendCat(){
+    return [...cats, 'Broom']
+};
+
+function prependCat(){
+    return ["Arnold", ...cats]
+};
+
+function removeLastCat(){
+    let catCopy = cats.slice(0, cats.length - 1);
+    return catCopy;
+};
+
+function removeFirstCat(){
+    let catCopy2 = cats.slice(1);
+    return catCopy2;
+}
